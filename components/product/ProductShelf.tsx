3e28bbc4c9e6b7cb663d6247672d89a8ce6e97c0
Thiago Pereira <thiago@brandnexity.com>
2024-05-10 16:11:40 -0300

    Create Component ProductShelf

--- components/product/ProductShelf.tsx
+++ components/product/ProductShelf.tsx
@@ -1,125 +1,225 @@
+import { LoaderReturnType } from "deco/mod.ts";
+import type { Layout as CardLayout } from "$store/components/product/ProductCard.tsx";
+import ProductCard from "$store/components/product/ProductCard.tsx";
+import {
+  CONDITIONAL_RESPONSIVE_PARAMS,
+  ResponsiveConditionals,
+} from "$store/components/ui/BannerCarousel.tsx";
+import Icon from "$store/components/ui/Icon.tsx";
+import Header from "$store/components/ui/SectionHeader.tsx";
+import Slider from "$store/components/ui/Slider.tsx";
+// import { SendEventOnLoad } from "$store/sdk/analytics.tsx";
+import { useOffer } from "$store/sdk/useOffer.ts";
 import type { Product } from "apps/commerce/types.ts";
 import { mapProductToAnalyticsItem } from "apps/commerce/utils/productToAnalyticsItem.ts";
-import { SendEventOnView } from "../../components/Analytics.tsx";
-import ProductCard from "../../components/product/ProductCard.tsx";
-import Icon from "../../components/ui/Icon.tsx";
-import Header from "../../components/ui/SectionHeader.tsx";
-import Slider from "../../components/ui/Slider.tsx";
-import { clx } from "../../sdk/clx.ts";
-import { useId } from "../../sdk/useId.ts";
-import { useOffer } from "../../sdk/useOffer.ts";
-import { usePlatform } from "../../sdk/usePlatform.tsx";
+import { useId } from "preact/hooks";
+import { HighLight } from "$store/components/product/ProductHighlights.tsx";
+import { DiscountBadgeProps } from "$store/components/product/DiscountBadge.tsx";
 
 export interface Props {
-  products: Product[] | null;
+  products: LoaderReturnType<Product[] | null>;
   title?: string;
-  description?: string;
-  layout?: {
-    numberOfSliders?: {
-      mobile?: 1 | 2 | 3 | 4 | 5;
-      desktop?: 1 | 2 | 3 | 4 | 5;
-    };
-    headerAlignment?: "center" | "left";
-    headerfontSize?: "Normal" | "Large" | "Small";
-    showArrows?: boolean;
+  highlights?: HighLight[];
+  seeMore?: {
+    url: string;
+    label: string;
   };
+  layout?: {
+    headerAlignment?: "center" | "left";
+    headerfontSize?: "Normal" | "Large";
+    itemsPerPage?: {
+      screenWidth?: number;
+      itemsQuantity?: number;
+    }[];
+  };
+  /**
+   * @title Show pagination arrows?
+   */
+  showPaginationArrows?: ResponsiveConditionals;
+  /**
+   * @title Show pagination dots?
+   * @default Always
+   */
+  showPaginationDots?: ResponsiveConditionals;
+  cardLayout?: CardLayout;
+}
+
+interface ButtonsProps {
+  className: string;
 }
 
 function ProductShelf({
   products,
   title,
-  description,
   layout,
+  cardLayout,
+  seeMore,
+  showPaginationArrows,
+  showPaginationDots,
+  highlights,
 }: Props) {
   const id = useId();
-  const platform = usePlatform();
 
   if (!products || products.length === 0) {
     return null;
   }
-  const slideDesktop = {
-    1: "md:w-full",
-    2: "md:w-1/2",
-    3: "md:w-1/3",
-    4: "md:w-1/4",
-    5: "md:w-1/5",
-  };
 
-  const slideMobile = {
-    1: "w-full",
-    2: "w-1/2",
-    3: "w-1/3",
-    4: "w-1/4",
-    5: "w-1/5",
-  };
   return (
-    <div class="w-full container py-8 flex flex-col gap-6 lg:py-10">
-      <Header
-        title={title || ""}
-        description={description || ""}
-        fontSize={layout?.headerfontSize || "Large"}
-        alignment={layout?.headerAlignment || "center"}
-      />
+    <div class="relative w-full py-8 mb-6 flex flex-col gap-12 lg:gap-7 lg:py-10">
+      <div class="flex items-center justify-between relative pb-3 border-b border-neutral-100">
+        <Header
+          title={title || ""}
+          description=""
+          fontSize={layout?.headerfontSize || "Large"}
+          alignment={layout?.headerAlignment || "center"}
+        />
+        {seeMore ? (
+          <span class="text-accent font-bold text-sm uppercase">
+            <a href={seeMore.url}>{seeMore.label}</a>
+          </span>
+        ) : null}
+      </div>
 
-      <div
-        id={id}
-        class={clx(
-          "grid",
-          layout?.showArrows && "grid-cols-[48px_1fr_48px]",
-          "px-0 md:px-5 container",
-        )}
-      >
-        <Slider class="carousel carousel-center sm:carousel-end sm:gap-1 row-start-2 row-end-5">
+      <div id={id} class="grid grid-cols-[48px_1fr_48px] px-0">
+        <Slider class="carousel carousel-start gap-6 col-span-full row-start-2 row-end-5">
           {products?.map((product, index) => (
-            <Slider.Item
-              index={index}
-              class={clx(
-                "carousel-item",
-                slideDesktop[layout?.numberOfSliders?.desktop ?? 3],
-                slideMobile[layout?.numberOfSliders?.mobile ?? 1],
-              )}
-            >
+            <Slider.Item index={index} class="carousel-item w-[270px]">
               <ProductCard
                 product={product}
                 itemListName={title}
+                layout={cardLayout}
+                highlights={highlights}
               />
             </Slider.Item>
           ))}
         </Slider>
 
-        {layout?.showArrows && (
-          <>
-            <div class="relative block z-10 col-start-1 row-start-3">
-              <Slider.PrevButton class="absolute w-12 h-12 flex justify-center items-center">
-                <Icon size={24} id="ChevronLeft" strokeWidth={3} class="w-5" />
-              </Slider.PrevButton>
-            </div>
-            <div class="relative block z-10 col-start-3 row-start-3">
-              <Slider.NextButton class="absolute w-12 h-12 flex justify-center items-center">
-                <Icon size={24} id="ChevronRight" strokeWidth={3} />
-              </Slider.NextButton>
-            </div>
-          </>
-        )}
-        <Slider.JS rootId={id} />
-        <SendEventOnView
-          id={id}
+        <Buttons
+          className={
+            CONDITIONAL_RESPONSIVE_PARAMS[
+              showPaginationArrows ? showPaginationArrows : "Always"
+            ]
+          }
+        />
+
+        <Dots
+          products={products}
+          className={
+            CONDITIONAL_RESPONSIVE_PARAMS[
+              showPaginationDots ? showPaginationDots : "Always"
+            ]
+          }
+        />
+
+        {/* <SendEventOnLoad
           event={{
             name: "view_item_list",
             params: {
               item_list_name: title,
               items: products.map((product, index) =>
                 mapProductToAnalyticsItem({
-                  index,
                   product,
+                  ...({ index: index }),
                   ...(useOffer(product.offers)),
                 })
               ),
             },
           }}
-        />
+        /> */}
+        <Slider.JS rootId={id} />
       </div>
     </div>
+  );
+}
+
+function Buttons({ className }: ButtonsProps) {
+  return (
+    <>
+      <div
+        class={`absolute top-2/4 lg:-left-11 left-2 z-10 col-start-1 row-start-3  ${className}`}
+      >
+        <Slider.PrevButton
+          style={{
+            minHeight: "28px",
+          }}
+          class="w-8 h-8 btn btn-circle opacity-100 bg-opacity-100  bg-neutral-100 border-none hover:bg-neutral-100"
+        >
+          <Icon
+            size={20}
+            id="ChevronLeft"
+            strokeWidth={3}
+            class="text-base-content"
+          />
+        </Slider.PrevButton>
+      </div>
+      <div
+        class={`absolute top-2/4 lg:-right-11 right-2  z-10 col-start-3 row-start-3 ${className}`}
+      >
+        <Slider.NextButton
+          style={{
+            minHeight: "28px",
+          }}
+          class="w-8 h-8 min-h-fit btn btn-circle opacity-100 bg-opacity-100  bg-neutral-100 border-none hover:bg-neutral-100"
+        >
+          <Icon
+            size={20}
+            id="ChevronRight"
+            strokeWidth={3}
+            class="text-base-content"
+          />
+        </Slider.NextButton>
+      </div>
+    </>
+  );
+}
+
+interface DotsProps {
+  products: LoaderReturnType<Product[] | null>;
+  /**
+   * @description Check this option when this banner is the biggest image on the screen for image optimizations
+   */
+  interval?: number;
+  /**
+   * @title Show pagination arrows?
+   */
+  className: string;
+}
+
+function Dots({ products, className, interval = 0 }: DotsProps) {
+  return (
+    <>
+      <style
+        dangerouslySetInnerHTML={{
+          __html: `
+          @property --dot-progress {
+            syntax: '<percentage>';
+            inherits: false;
+            initial-value: 0%;
+          }
+          `,
+        }}
+      />
+      <ul
+        class={`carousel absolute w-full left-0 -bottom-4 justify-center col-span-full gap-2 z-10 row-start-4  ${className}`}
+      >
+        {products?.map((_, index) => (
+          <li class="carousel-item">
+            <Slider.Dot index={index}>
+              <div
+                class={`py-5 ${
+                  index === 0 || index % 4 === 0 ? "" : "lg:hidden"
+                }`}
+              >
+                <div
+                  class="w-3 h-3 group-disabled:opacity-100 opacity-10 rounded-full bg-primary"
+                  style={{ animationDuration: `${interval}s` }}
+                />
+              </div>
+            </Slider.Dot>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
