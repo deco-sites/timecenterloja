26fe86478d24e7ef7dc06432dfa115db11e43d3d
Thiago Pereira <thiago@brandnexity.com>
2024-07-01 14:10:15 -0300

    Seo block custom

---
+++ components/seo/SeoPLP.tsx
@@ -0,0 +1,44 @@
+import ScriptLDJson from "$store/components/seo/ScriptLDJson.tsx";
+import type { ProductListingPage } from "apps/commerce/types.ts";
+import { LoaderReturnType } from "deco/mod.ts";
+import BaseSeo from "./SeoBase.tsx";
+
+export interface Props {
+  page: LoaderReturnType<ProductListingPage | null>;
+  /**
+   * @title Title template
+   * @description add a %s whenever you want it to be replaced with the product name
+   * @default %s | Fashion Store
+   */
+  titleTemplate: string;
+  title?: string;
+  description?: string;
+  url?: string;
+  imageUrl?: string;
+  themeColor?: string;
+}
+
+function SeoPLP({ page, ...baseSeo }: Props) {
+  const breadcrumbList = page?.breadcrumb;
+
+  let title = baseSeo?.title;
+  if (page?.seo?.title) {
+    title = baseSeo?.titleTemplate?.replace("%s", page?.seo?.title || "") ||
+      page?.seo?.title;
+  }
+
+  return (
+    <>
+      <BaseSeo
+        {...{
+          ...baseSeo,
+          title,
+          description: page?.seo?.description ?? baseSeo?.description,
+        }}
+      />
+      <ScriptLDJson {...breadcrumbList} />
+    </>
+  );
+}
+
+export default SeoPLP;
--- manifest.gen.ts
+++ manifest.gen.ts
@@ -76,9 +76,10 @@ import * as $$$$$$64 from "./sections/Product/ProductShelfTabbed.tsx";
 import * as $$$$$$65 from "./sections/Product/SearchResult.tsx";
 import * as $$$$$$66 from "./sections/Product/ShelfWithImage.tsx";
 import * as $$$$$$67 from "./sections/Product/Wishlist.tsx";
-import * as $$$$$$68 from "./sections/Social/InstagramPosts.tsx";
-import * as $$$$$$69 from "./sections/Social/WhatsApp.tsx";
-import * as $$$$$$70 from "./sections/Theme/Theme.tsx";
+import * as $$$$$$68 from "./sections/Seo/SeoPLPV5.tsx";
+import * as $$$$$$69 from "./sections/Social/InstagramPosts.tsx";
+import * as $$$$$$70 from "./sections/Social/WhatsApp.tsx";
+import * as $$$$$$71 from "./sections/Theme/Theme.tsx";
 
 const manifest = {
   "loaders": {
@@ -156,9 +157,10 @@ const manifest = {
     "site/sections/Product/SearchResult.tsx": $$$$$$65,
     "site/sections/Product/ShelfWithImage.tsx": $$$$$$66,
     "site/sections/Product/Wishlist.tsx": $$$$$$67,
-    "site/sections/Social/InstagramPosts.tsx": $$$$$$68,
-    "site/sections/Social/WhatsApp.tsx": $$$$$$69,
-    "site/sections/Theme/Theme.tsx": $$$$$$70,
+    "site/sections/Seo/SeoPLPV5.tsx": $$$$$$68,
+    "site/sections/Social/InstagramPosts.tsx": $$$$$$69,
+    "site/sections/Social/WhatsApp.tsx": $$$$$$70,
+    "site/sections/Theme/Theme.tsx": $$$$$$71,
   },
   "apps": {
     "site/apps/decohub.ts": $$$$$$$$$$$0,
