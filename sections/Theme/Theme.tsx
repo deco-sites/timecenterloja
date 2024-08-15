08e2ec2c5874cff6f4fdfed0bf5f6f8729a91757
Marcus <marcus.castro@agencian1.com.br>
2024-08-15 16:53:28 -0300

    10951 - fix: ajuste na fonte Gothan

--- sections/Theme/Theme.tsx
+++ sections/Theme/Theme.tsx
@@ -263,7 +263,7 @@ function Section({
     [
       "--font-family",
       font?.family ||
-      " -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen-Sans, Ubuntu, Cantarell, 'Helvetica Neue', sans-serif",
+      "'Gotham', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen-Sans, Ubuntu, Cantarell, 'Helvetica Neue', sans-serif",
     ],
   ]
     .map(([name, value]) => ({ name, value }));
