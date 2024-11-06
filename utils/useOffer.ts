cbe15666ec669edbb115da3720693dd1dc9d89a0
André Rodrigues <andre.rodrigues@agencian1.com.br>
2024-11-06 12:48:34 -0400

    11282 - code format

--- utils/useOffer.ts
+++ utils/useOffer.ts
@@ -1,14 +1,14 @@
 import type {
   AggregateOffer,
   UnitPriceSpecification,
-} from "apps/commerce/types.ts";
-import { formatPrice } from "$store/sdk/format.ts";
+} from 'apps/commerce/types.ts';
+import { formatPrice } from '$store/sdk/format.ts';
 
 const bestInstallment = (
   accumulator: UnitPriceSpecification | null,
   current: UnitPriceSpecification,
 ) => {
-  if (current.priceComponentType !== "https://schema.org/Installment") {
+  if (current.priceComponentType !== 'https://schema.org/Installment') {
     return accumulator;
   }
 
@@ -27,25 +27,25 @@ const installmentToString = (installment: UnitPriceSpecification) => {
   const { billingDuration, billingIncrement } = installment;
 
   if (!billingDuration || !billingIncrement) {
-    return "";
+    return '';
   }
 
-  return `${billingDuration}x de ${formatPrice(billingIncrement, "BRL")}`;
+  return `${billingDuration}x de ${formatPrice(billingIncrement, 'BRL')}`;
 };
 
 export const useOffer = (aggregateOffer?: AggregateOffer) => {
   const offer = aggregateOffer?.offers[0];
 
   const listPrice = offer?.priceSpecification.find(
-    ({ priceType }) => priceType === "https://schema.org/ListPrice",
+    ({ priceType }) => priceType === 'https://schema.org/ListPrice',
   );
 
   const sellerPrice = offer?.priceSpecification.find(
-    ({ priceType }) => priceType === "https://schema.org/SalePrice",
+    ({ priceType }) => priceType === 'https://schema.org/SalePrice',
   );
 
   const priceWithPixPayment = offer?.priceSpecification.find(
-    ({ name }) => name?.toLowerCase() === "pix",
+    ({ name }) => name?.toLowerCase() === 'pix',
   );
 
   const installment = offer?.priceSpecification.reduce(bestInstallment, null);
@@ -54,9 +54,10 @@ export const useOffer = (aggregateOffer?: AggregateOffer) => {
   const availability = (offer?.inventoryLevel.value || 0) > 0;
   const manualPixPercentDiscount = 5;
 
-  const priceWithPixDiscount = (priceWithPixPayment?.price || price) < price
-    ? priceWithPixPayment?.price || price
-    : price * ((100 - manualPixPercentDiscount) / 100);
+  const priceWithPixDiscount =
+    (priceWithPixPayment?.price || price) < price
+      ? priceWithPixPayment?.price || price
+      : price * ((100 - manualPixPercentDiscount) / 100);
 
   const pixPercentDiscountByDiferenceSellerPrice = Math.round(
     100 - (priceWithPixDiscount * 100) / price,
