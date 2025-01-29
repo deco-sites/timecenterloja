48e6bfea125e9068d63fc5a7dd5d6f1a974f7a3e
André Rodrigues <andre.rodrigues@agencian1.com.br>
2025-01-29 17:23:56 -0400

    11604 - change SeoPlpCustomV2 section

--- sections/Seo/SeoPlpCustomV2.tsx
+++ sections/Seo/SeoPlpCustomV2.tsx
@@ -4,27 +4,26 @@ import {
   Props as SeoPlpV2Props,
   loader as seoPlpV2Loader,
 } from 'apps/commerce/sections/Seo/SeoPLPV2.tsx';
-import { fix_data_struct_by_pix_payment } from 'deco-sites/timecenter/sdk/schema_org.ts';
 
 export interface Props extends SeoPlpV2Props {}
 
 /** @title PLP Custom V2 */
 export function loader(props: Props, req: Request, ctx: AppContext) {
   const plp_seo_deco = seoPlpV2Loader(props, req, ctx);
-  const plp_seo_with_pix_discount = plp_seo_deco.jsonLDs.map((json_ld) => {
-    if (json_ld && json_ld.products && json_ld.products.length > 0) {
-      const new_products = json_ld.products.map((product) =>
-        fix_data_struct_by_pix_payment(product),
-      );
-      json_ld.products = new_products;
-    }
-    return json_ld;
-  });
 
-  return {
-    ...plp_seo_deco,
-    jsonLDs: plp_seo_with_pix_discount,
-  };
+  if (
+    plp_seo_deco.jsonLDs &&
+    plp_seo_deco.jsonLDs.length > 0 &&
+    plp_seo_deco.jsonLDs[0] &&
+    plp_seo_deco.jsonLDs[0].breadcrumb
+  ) {
+    return {
+      ...plp_seo_deco,
+      jsonLDs: [plp_seo_deco.jsonLDs[0].breadcrumb],
+    };
+  }
+
+  return { plp_seo_deco };
 }
 
 export function LoadingFallback(props: Partial<Props>) {
