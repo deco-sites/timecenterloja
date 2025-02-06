8c5fd5af37078862af5a828a18b44872fc5e15df
André Rodrigues <andre.rodrigues@agencian1.com.br>
2025-02-06 13:26:33 -0400

    11604_v2 - create schema_org.ts

---
+++ sdk/schema_org.ts
@@ -0,0 +1,33 @@
+import { Product } from "apps/commerce/types.ts";
+import { useOffer } from "deco-sites/timecenter/utils/useOffer.ts";
+
+export function fix_data_struct_by_pix_payment(product: Product) {
+  const { listPrice, priceWithPixDiscount } = useOffer(product.offers);
+
+  if (product.offers?.highPrice) {
+    product.offers.highPrice = listPrice;
+  }
+
+  if (product.offers?.lowPrice) {
+    product.offers.lowPrice = priceWithPixDiscount;
+  }
+
+  if (product.offers?.offers) {
+    if (
+      product.offers.offers[0].priceSpecification[0].priceType ===
+        "https://schema.org/ListPrice"
+    ) {
+      product.offers.offers[0].priceSpecification[0].price = listPrice;
+    }
+
+    if (
+      product.offers.offers[0].priceSpecification[1].priceType ===
+        "https://schema.org/SalePrice"
+    ) {
+      product.offers.offers[0].priceSpecification[1].price =
+        priceWithPixDiscount;
+    }
+  }
+
+  return product;
+}
