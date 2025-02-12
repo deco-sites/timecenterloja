c27fab7f6b451e3a2ca6d25da46d7dec8cc5f61e
André Rodrigues <andre.rodrigues@agencian1.com.br>
2025-02-12 11:21:27 -0400

    11604_v2 - change SeoBaseCustomV2

--- sections/Seo/SeoBaseCustomV2.tsx
+++ sections/Seo/SeoBaseCustomV2.tsx
@@ -5,19 +5,8 @@ import Seo, {
 } from 'apps/website/components/Seo.tsx';
 import { loader as seoV2Loader } from 'apps/website/sections/Seo/SeoV2.tsx';
 
-type Props = Pick<
-  SeoDecoProps,
-  | 'title'
-  | 'description'
-  | 'type'
-  | 'favicon'
-  | 'image'
-  | 'themeColor'
-  | 'noIndexing'
->;
-
 /** @title Base Custom V2 */
-export function loader(props: Props, req: Request, ctx: AppContext) {
+export function loader(props: SeoDecoProps, req: Request, ctx: AppContext) {
   const seo_base_deco = seoV2Loader(props, req, ctx);
   const url = new URL(req.url);
   const new_canonical = `${url.origin}${url.pathname}`;
@@ -28,11 +17,11 @@ export function loader(props: Props, req: Request, ctx: AppContext) {
   };
 }
 
-export function LoadingFallback(props: Partial<Props>) {
+export function LoadingFallback(props: Partial<SeoDecoProps>) {
   return <Seo {...props} />;
 }
 
-export default function Section(props: Props): SEOSection {
+export default function Section(props: SeoDecoProps): SEOSection {
   return <Seo {...props} />;
 }
 
