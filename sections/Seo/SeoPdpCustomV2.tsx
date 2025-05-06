31691f02574e6ad8be8abe549d4c6709d8172585
thiagon1 <thiago.pereira@n1.ag>
2025-05-06 20:36:17 -0300

    Fix Deco

--- sections/Seo/SeoPdpCustomV2.tsx
+++ sections/Seo/SeoPdpCustomV2.tsx
@@ -1,55 +1,39 @@
 import { AppContext } from 'apps/commerce/mod.ts';
-import type { SectionProps } from 'deco/types.ts';
-import {
-  loader as seoPdpV2Loader,
-  Props as SeoPdpV2Props,
-} from 'apps/commerce/sections/Seo/SeoPDPV2.tsx';
+import { loader as seoPdpV2Loader, Props as SeoPdpV2Props, } from 'apps/commerce/sections/Seo/SeoPDPV2.tsx';
 import { fix_data_struct_by_pix_payment } from 'deco-sites/timecenter/sdk/schema_org.ts';
 import { SEOSection } from 'apps/website/components/Seo.tsx';
 import SeoBaseCustomV2 from 'deco-sites/timecenter/sections/Seo/SeoBaseCustomV2.tsx';
-
-export interface Props extends SeoPdpV2Props {}
-
+import { type SectionProps } from "@deco/deco";
+export interface Props extends SeoPdpV2Props {
+}
 /** @title PDP Custom V2 */
 export function loader(props: Props, req: Request, ctx: AppContext) {
-  const pdp_seo_deco = seoPdpV2Loader(props, req, ctx);
-  const pdp_seo_with_pix_discount = pdp_seo_deco.jsonLDs.map((json_ld) => {
-    if (json_ld && json_ld.product) {
-      const new_json_ld = {
-        ...json_ld,
-        product: fix_data_struct_by_pix_payment(json_ld.product),
-      };
-      json_ld = new_json_ld;
-    }
-    return json_ld;
-  });
-
-  delete pdp_seo_deco.canonical;
-
-  const url_formatted = new URL(req.url);
-  const has_url_query_string = url_formatted.search !== '';
-
-  return {
-    ...pdp_seo_deco,
-    jsonLDs: pdp_seo_with_pix_discount,
-    has_url_query_string,
-    titleTemplate: (ctx.seo && ctx.seo.titleTemplate) || '',
-    descriptionTemplate: (ctx.seo && ctx.seo.descriptionTemplate) || '',
-  };
+    const pdp_seo_deco = seoPdpV2Loader(props, req, ctx);
+    const pdp_seo_with_pix_discount = pdp_seo_deco.jsonLDs.map((json_ld) => {
+        if (json_ld && json_ld.product) {
+            const new_json_ld = {
+                ...json_ld,
+                product: fix_data_struct_by_pix_payment(json_ld.product),
+            };
+            json_ld = new_json_ld;
+        }
+        return json_ld;
+    });
+    delete pdp_seo_deco.canonical;
+    const url_formatted = new URL(req.url);
+    const has_url_query_string = url_formatted.search !== '';
+    return {
+        ...pdp_seo_deco,
+        jsonLDs: pdp_seo_with_pix_discount,
+        has_url_query_string,
+        titleTemplate: (ctx.seo && ctx.seo.titleTemplate) || '',
+        descriptionTemplate: (ctx.seo && ctx.seo.descriptionTemplate) || '',
+    };
 }
-
 export function LoadingFallback(props: Partial<SectionProps<typeof loader>>) {
-  return (
-    <SeoBaseCustomV2
-      {...{ ...props, has_url_query_string: !!props.has_url_query_string }}
-    />
-  );
+    return (<SeoBaseCustomV2 {...{ ...props, has_url_query_string: !!props.has_url_query_string }}/>);
 }
-
-export default function Section(
-  props: SectionProps<typeof loader>,
-): SEOSection {
-  return <SeoBaseCustomV2 {...props} />;
+export default function Section(props: SectionProps<typeof loader>): SEOSection {
+    return <SeoBaseCustomV2 {...props}/>;
 }
-
 export { default as Preview } from 'apps/website/components/_seo/Preview.tsx';
